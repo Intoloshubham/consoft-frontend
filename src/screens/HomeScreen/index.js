896afac2a3e8219073667ff357a608b9c4f42a86
Intoloshubham <shubham.sahu@intoloindia.com>
2022-05-23 12:01:37 +0545

    changes to home

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -13,7 +13,7 @@ const HomeScreen = () => {
   return (
     <View>
       <Text style={{fontSize:24, alignSelf:'center'}}>index home screen</Text>
-      <Text onPress={onSignInPressed} > Sign In  agai dsfs   fdgdfg n gfdg
+      <Text onPress={onSignInPressed} > Sign In  
       fdgdf </Text>
     </View>
   )
