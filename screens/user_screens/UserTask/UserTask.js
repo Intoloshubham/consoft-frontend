88806abdb83ea463b28028a04d61b6d459c8e87a
SddOffice <ssdoffice44@gmail.com>
2022-05-27 10:27:22 +0530

    create user task

---
+++ screens/user_screens/UserTask/UserTask.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native';
+
+function UserTask() {
+  return (
+    <div>
+      <Text>User Task</Text>
+    </div>
+  )
+}
+
+export default UserTask
