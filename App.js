747a27b1b24f227f2127d26fc891812c1b3b6d0d
Intoloshubham <shubham.sahu@intoloindia.com>
2022-05-28 18:17:43 +0545

    pushed my task work

--- App.js
+++ App.js
@@ -15,16 +15,10 @@ import {
   ProjectSeheduleTime,
   Account,
 } from './screens/admin_screens';
-<<<<<<< HEAD
-import { UserDashboard } from './screens/user_screens';
-import Tabs from './navigation/tabs';
-import { Account } from './screens/admin_screens';
-
-=======
-import {Profile, Demo, Demo1, Demo2} from './screens/user_screens';
+import { Profile, Demo, Demo1, Demo2 } from './screens/user_screens';
 import Tabs from './navigation/tabs';
 import UserTabs from './navigation/user_tabs';
->>>>>>> 2c71bf80a9f1a559d3ce4bf38cb550c9d9d39582
+
 
 
 const Stack = createStackNavigator();
@@ -35,7 +29,6 @@ const App = () => {
   }, []);
 
   return (
-<<<<<<< HEAD
     <ApplicationProvider {...eva} theme={eva.light}>
       <NavigationContainer>
         <Stack.Navigator
@@ -43,7 +36,7 @@ const App = () => {
             headerShown: false,
           }}
           initialRouteName={'OnBoarding'}>
-          <Stack.Screen name="Home" component={Tabs} />      
+          <Stack.Screen name="Home" component={Tabs} />
           <Stack.Screen name="OnBoarding" component={OnBoarding} />
           <Stack.Screen name="SignIn" component={SignIn} />
           <Stack.Screen name="SignUp" component={SignUp} />
@@ -58,7 +51,12 @@ const App = () => {
           <Stack.Screen name="ToolsAndMachinery" component={ToolsAndMachinery} />
 
           {/* User  */}
-          <Stack.Screen name="UserDashboard" component={UserDashboard} />
+          <Stack.Screen name="UserDashboard" component={UserTabs} />
+          <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="Demo" component={Demo} />
+          <Stack.Screen name="Demo1" component={Demo1} />
+          <Stack.Screen name="Demo2" component={Demo2} />
+
           <Stack.Screen
             name="ProjectSeheduleTime"
             component={ProjectSeheduleTime}
@@ -66,41 +64,6 @@ const App = () => {
         </Stack.Navigator>
       </NavigationContainer>
     </ApplicationProvider>
-=======
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName={'OnBoarding'}>
-        <Stack.Screen name="Home" component={Tabs} />
-        <Stack.Screen name="OnBoarding" component={OnBoarding} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="Otp" component={Otp} />
-        <Stack.Screen name="Account" component={Account} />
-        <Stack.Screen name="ProjectsDetails" component={ProjectsDetails} />
-        <Stack.Screen name="ProjectTeam" component={ProjectTeam} />
-        <Stack.Screen name="Contractors" component={Contractors} />
-        <Stack.Screen name="StocksAndInventry" component={StocksAndInventry} />
-        <Stack.Screen name="ProjectReports" component={ProjectReports} />
-        <Stack.Screen name="ToolsAndMachinery" component={ToolsAndMachinery} />
-
-        {/* User  */}
-        <Stack.Screen name="UserDashboard" component={UserTabs} />
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="Demo" component={Demo} />
-        <Stack.Screen name="Demo1" component={Demo1} />
-        <Stack.Screen name="Demo2" component={Demo2} />
-
-        <Stack.Screen
-          name="ProjectSeheduleTime"
-          component={ProjectSeheduleTime}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
->>>>>>> 2c71bf80a9f1a559d3ce4bf38cb550c9d9d39582
   );
 };
 
--- screens/user_screens/Reports/Reports.js
+++ screens/user_screens/Reports/Reports.js
@@ -1,27 +1,37 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import DataTable, { COL_TYPES } from 'react-native-datatable-component'
-
+import { icons, COLORS, SIZES, FONTS, dummyData } from '../../../constants';
 function Qty_eng_section() {
   return (
-    <View>
+    <View style={{ width: '95%', alignSelf: 'center' }}>
       <DataTable
         data={[
-          { Sno: 1, Particular: 12 },
-          { Sno: 2, Particular: 22 },
-          { Sno: 3, Particular: 21 },
-          { Sno: 4, Particular: 22 },
-          { Sno: 5, Particular: 20 },
-          { Sno: 6, Particular: 13 }
+          { Sno: 1, Particulars: 12, select: false },
+          { Sno: 2, Particulars: 22, select: true, doHighlight: 'green' },
+          { Sno: 3, Particulars: 21, select: false },
+          { Sno: 4, Particulars: 22, select: false },
+          { Sno: 5, Particulars: 20, select: false },
+          { Sno: 6, Particulars: 13, select: false }
         ]} // list of objects
-        colNames={['Sno', 'Particular']} //List of Strings
+        colNames={['Sno', 'Particulars', 'L', 'B', 'H','select' ]} //List of Strings
+
         colSettings={[
-          { name: 'Sno', type: COL_TYPES.INT, width: '50%' },
-          { name: 'Particular', type: COL_TYPES.STRING, width: '30%' }
+          {
+            name: 'Sno',
+            type: COL_TYPES.INT,
+            width: '13%',
+        
+          },
+          { name: 'Particulars', type: COL_TYPES.STRING, width: '35%' },
+          { name: 'select', type: COL_TYPES.CHECK_BOX, width: '20%', doHighlight: { backgroundColor: '#917584', textColor: 'red' } },
+          { name: 'L', type: COL_TYPES.INT, width: '10%' },
+          { name: 'B', type: COL_TYPES.INT, width: '10%' },
+          { name: 'H', type: COL_TYPES.INT, width: '10%' },
         ]}//List of Objects
         noOfPages={2} //number
-        backgroundColor={'#E4E9F2'} //Table Background Color
-        headerLabelStyle={{ color: '#000', fontSize: 12 }} //Text Style Works
+        backgroundColor={COLORS.lightblue_500} //Table Background Color
+        headerLabelStyle={{ color: '#000', fontSize: 15 }} //Text Style Works
       />
     </View>
   )
@@ -41,7 +51,6 @@ function Stock_keeper_section() {
 export default function Reports() {
   return (
     <View>
-      <Text>Reports</Text>
       {Qty_eng_section()}
       {Quality_eng_section()}
       {Stock_keeper_section()}
