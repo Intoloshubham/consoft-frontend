f221b4aa3f9d0b9b8d045fa85f046b5f43bb8c7f
shivamsoni786 <saurabhsoni1101@gmail.com>
2022-05-28 18:33:36 +0530

    consoft logo

--- constants/images.js
+++ constants/images.js
@@ -7,6 +7,7 @@ const logo_01 = require('../assets/images/logo_01.png');
 const logo_02 = require('../assets/images/logo_02.png');
 const profile = require('../assets/images/profile.png');
 const cons_logo = require('../assets/images/cons_logo.png');
+const conSoftLogo = require('../assets/images/conSoftLogo.jpg');
 const building_01 = require('../assets/images/building_01.png');
 const back_banner_01 = require('../assets/images/back_banner_01.png');
 const backg_01 = require('../assets/images/backg_01.png');
@@ -15,6 +16,10 @@ const Profile7 = require('../assets/images/Profile7.jpg');
 const green = require('../assets/images/green.png');
 const yellow = require('../assets/images/yellow.png');
 const red = require('../assets/images/red.png');
+
+const b1 = require('../assets/images/building/b1.png');
+const b2 = require('../assets/images/building/b2.png');
+const b3 = require('../assets/images/building/b3.png');
 
 export default {
   background_01,
@@ -34,4 +39,8 @@ export default {
   green,
   yellow,
   red,
+  conSoftLogo,
+  b1,
+  b2,
+  b3,
 };
