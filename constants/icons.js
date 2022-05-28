48625e9b976d4836f513177df8ee0f7ae7507715
SddOffice <ssdoffice44@gmail.com>
2022-05-28 16:46:27 +0530

    push by dev

--- constants/icons.js
+++ constants/icons.js
@@ -39,12 +39,8 @@ const dashboard = require('../assets/icons/dashboard.png');
 const tracker = require('../assets/icons/tracker.png');
 const right_arr = require('../assets/icons/right_arr.png');
 const back = require('../assets/icons/back.png');
-<<<<<<< HEAD
-const comp_tasks = require('../assets/icons/completed.png');
-=======
 const sun = require('../assets/icons/sun.png');
 const camera = require('../assets/icons/camera.png');
->>>>>>> 2c71bf80a9f1a559d3ce4bf38cb550c9d9d39582
 //pd
 const stock = require('../assets/icons/pd/stock.png');
 const report1 = require('../assets/icons/pd/report.png');
@@ -107,7 +103,6 @@ export default {
   machine,
   p_team,
   contr,
-  comp_tasks,
   mall,
   apartment,
   garage,
