7fbd2512764017d520192ff89e0aabae453da905
shivamsoni786 <saurabhsoni1101@gmail.com>
2022-05-29 17:12:29 +0530

    account unit & items

--- screens/admin_screens/Stock/Items/Items.js
+++ screens/admin_screens/Stock/Items/Items.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {HeaderBar} from '../../../../Components';
 
 const Items = () => {
   return (
     <View>
+      <HeaderBar right={true} />
       <Text>Items stock</Text>
     </View>
   );
