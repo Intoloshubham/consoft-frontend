fd933d59ae501fdc2b4fd08c3f8fb62f8cf2c44b
Intoloshubham <shubham.sahu@intoloindia.com>
2022-06-01 19:20:14 +0545

    rEPORT COMIT

--- screens/user_screens/UserReports/UserReports.js
+++ screens/user_screens/UserReports/UserReports.js
@@ -1,33 +1,42 @@
 import React from 'react'
 import { View, Text, FlatList, StyleSheet, Image, ScrollView, Modal, Pressable, TouchableHighlight, TouchableOpacity } from 'react-native'
-import { COLORS, FONTS, SIZES,dummyData } from '../../../constants'
+import { COLORS, FONTS, SIZES, dummyData } from '../../../constants'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import { CheckBox, Layout, Card } from '@ui-kitten/components';
+import { Divider } from '@ui-kitten/components';
+
 
 
 // FontAwesome5.loadFont();
 
+
+
 const UserReports = () => {
+  const [selectedId,setSelectedId] = React.useState(null)
 
-  
-// const Item = ({ item, onPress, backgroundColor, textColor }) => (
-//   <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-//     <Text style={[styles.title, textColor]}>{item.title}</Text>
-//   </TouchableOpacity>
-// );
+  const OnSelectedActiveItem = (activeItem) => {
 
-  // const renderItem = ({ item }) => {
-  //   const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
-  //   const color = item.id === selectedId ? 'white' : 'black';
+    console.log(activeItem);
+    setSelectedId(activeItem)
+}
 
-  //   return (
-  //     <Item
-  //       item={item}
-  //       onPress={() => setSelectedId(item.id)}
-  //       backgroundColor={{ backgroundColor }}
-  //       textColor={{ color }}
-  //     />
-  //   );
-  // };
+
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <TouchableOpacity  
+      // style={{backgroundColor:COLORS.transparentBlack1,padding:SIZES.largeTitle}}
+      style={[styles.rend_rep_card,{   backgroundColor: (selectedId?.id == item.id) ? "#26D1B2" : COLORS.gray3}]}
+      onPress={() => OnSelectedActiveItem(item)}
+      >
+        <Text style={{...FONTS.body4}}>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
 
   return (
     <>
@@ -35,35 +44,41 @@ const UserReports = () => {
         <View >
           <Text style={{ ...FONTS.h2, color: COLORS.blue }}>Manpower</Text>
         </View>
-        <View style={{ flexDirection: "row", backgroundColor:"gray",paddingHorizontal:5 }}>
-          <View style={{backgroundColor:"green"}}>
-            <Pressable>
-              <FontAwesome5 name='arrow-circle-left' size={SIZES.h3} />
-            </Pressable>
-          </View>
-          <View style={{backgroundColor:"red"}}>
-            <Pressable>
-              <FontAwesome5 name='arrow-circle-right' size={SIZES.h3} />
-            </Pressable>
-          </View>
+        <View style={{ flexDirection: "row", backgroundColor: COLORS.white2, paddingHorizontal: SIZES.base, marginHorizontal: -SIZES.padding * 0.95, top: SIZES.base }}>
+          <Pressable style={{ marginRight: 5 }}>
+            <FontAwesome5 name='arrow-circle-left' size={SIZES.h2} />
+          </Pressable>
+          <Pressable>
+            <FontAwesome5 name='arrow-circle-right' size={SIZES.h2} />
+          </Pressable>
         </View>
       </View>
-      <Text>Total Reports</Text>
+      <Text
+        style={{
+          paddingHorizontal: SIZES.h3 * 2,
+          paddingVertical: SIZES.base,
+          ...FONTS.body5,
+          color: COLORS.red
+        }}
+
+      >Ongoing 8 Reports</Text>
+      <Divider style={{ backgroundColor: COLORS.black, marginHorizontal: 8 }} />
       <View>
-      {/* <FlatList
-        data={dummyData.Reports_part}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
-      /> */}
+        <FlatList
+          data={dummyData.Reports_part}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          extraData={selectedId}
+          horizontal
+        />
       </View>
-      <Text>card divider</Text>
-      {/* some space then start selected report sections */}
       <Text>Name of selected Report</Text>
       <View>
         <Text>Give + icon and tag </Text>
       </View>
-      <Text>card divider</Text>
+      <Divider style={{ backgroundColor: COLORS.black, marginHorizontal: 8 }} />
+      {/* some space then start selected report sections */}
+
       <View>
         <Text>section/container of selected report </Text>
       </View>
@@ -72,3 +87,11 @@ const UserReports = () => {
 }
 
 export default UserReports
+
+const styles=StyleSheet.create({
+
+  rend_rep_card:{
+    padding:SIZES.body1,
+    margin:5
+  }
+})
