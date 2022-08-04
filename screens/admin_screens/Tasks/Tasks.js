4552a33d6c80a9fce9c5a5dd59b2f7d41ec42ac6
shivamsoni786 <saurabhsoni1101@gmail.com>
2022-08-04 10:40:04 +0530

    changes

--- screens/admin_screens/Tasks/Tasks.js
+++ screens/admin_screens/Tasks/Tasks.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import {Text, View, Alert, TouchableOpacity} from 'react-native';
 import {COLORS, FONTS, icons} from '../../../constants';
-import {DeleteConfirmationToast} from '../../../Components';
 
 const Tasks = () => {
-  const [modal, setModal] = React.useState(false);
-
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <TouchableOpacity onPress={() => setModal(true)}>
-        <Text style={{...FONTS.h2, color: COLORS.darkGray}}>Tasks</Text>
-      </TouchableOpacity>
-      <DeleteConfirmationToast
-        isVisible={modal}
-        onClose={() => setModal(false)}
-        title={'Are You Sure?'}
-        message={'Do you really want to delete?'}
-        color={COLORS.rose_600}
-        icon={icons.delete_withbg}
-        onClickYes={() => alert('delete')}
-      />
+      <Text style={{...FONTS.h2, color: COLORS.darkGray}}>Tasks</Text>
     </View>
   );
 };
