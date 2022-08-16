6452a5daa5302db6e1874c57168419fd05237b20
Intolorahul <rahul.nath@intoloindia.com>
2022-08-16 18:07:58 +0530

    unit item

---
+++ controller/ItemsController.js
@@ -0,0 +1,98 @@
+import Config from '../config';
+
+const submitItem = async dataitem => {
+  try {
+    const res = await fetch(Config.API_URL + 'item', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataitem),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//   unit save api
+const saveUnitname = async unitdata => {
+  try {
+    const res = await fetch(Config.API_URL + 'unit', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(unitdata),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUnits = async () => {
+  try {
+    const res = await fetch(`${Config.API_URL}unit`, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getItems = async () => {
+  try {
+    const res = await fetch(`${Config.API_URL}item`, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateItems = async (itemid, updateItemdata) => {
+  try {
+    const res = await fetch(`${Config.API_URL}item/` + itemid, {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updateItemdata),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteitems = async id => {
+  // console.log('object', id);
+  try {
+    const res = await fetch(Config.API_URL + 'item/' + id, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {submitItem, getUnits, saveUnitname, getItems, updateItems, deleteitems};
--- controller/unitController.js
+++ controller/unitController.js
@@ -15,4 +15,52 @@ const getUnits = async () => {
   }
 };
 
-export {getUnits};
+const postunits = async unitdata => {
+  try {
+    const res = await fetch(Config.API_URL + 'unit', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(unitdata),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const unitdalete = async id => {
+  // console.log('object', id);
+  try {
+    const res = await fetch(Config.API_URL + 'unit/' + id, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateunitname = async (unitid, updateunitdata) => {
+  try {
+    const res = await fetch(`${Config.API_URL}unit/` + unitid, {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updateunitdata),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {getUnits,postunits,unitdalete,updateunitname};
