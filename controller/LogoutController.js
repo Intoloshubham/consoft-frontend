b317847a8805a735a31fcfc93a9a956ffe40b59c
shivamsoni786 <saurabhsoni1101@gmail.com>
2022-10-07 19:27:09 +0530

    changes

---
+++ controller/LogoutController.js
@@ -0,0 +1,19 @@
+import Config from '../config';
+
+const postLogout = async formData => {
+  try {
+    const res = await fetch(Config.API_URL + 'logout', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {postLogout};
--- controller/UserAttendanceController.js
+++ controller/UserAttendanceController.js
@@ -1,12 +1,59 @@
 import Config from '../config';
 
-const getUserAttendance = async company_id => {
+const getUserAttendance = async (company_id, year, month, user_id) => {
   try {
-    const res = await fetch(Config.API_URL + 'attendance/' + company_id, {
-      method: 'get',
+    const res = await fetch(
+      Config.API_URL +
+        'attendance/' +
+        company_id +
+        '/' +
+        year +
+        '/' +
+        month +
+        '/' +
+        user_id,
+      {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getCheckUserPresent = async (company_id, user_id) => {
+  // console.log("id1",company_id);
+  // console.log("id2",user_id);
+  try {
+    const res = await fetch(
+      Config.API_URL + 'check-present/' + company_id + '/' + user_id,
+      {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const postUserAttendance = async formData => {
+  try {
+    const res = await fetch(Config.API_URL + 'attendance', {
+      method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
+      body: JSON.stringify(formData),
     });
     const data = await res.json();
     return data;
@@ -15,4 +62,4 @@ const getUserAttendance = async company_id => {
   }
 };
 
-export {getUserAttendance};
+export {getUserAttendance, postUserAttendance, getCheckUserPresent};
